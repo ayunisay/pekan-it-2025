f534bc546bf210973d999e5209a5e5064369d310
aryarobyy <robyaryanata12345@gmail.com>
2025-06-21 10:19:16 +0700

    profie page ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router"
+import { Route, Routes, useLocation } from "react-router"
 import { Dashboard } from "./pages/Dashboard"
 import Auth from "./pages/auth/Auth"
 import Register from "./pages/auth/Register"
@@ -7,11 +7,21 @@ import Profile from "./pages/profile/Profile"
 import PomodoroTimer from "./pages/pomodoro/Pomodoro"
 import Navbar from "./components/Navbar"
 import Footer from "./components/Footer"
+import FindFriend from "./pages/FindFriend"
 
 function App() {
+  const location = useLocation();
+  const pathCheck = location.pathname === '/profile' || location.pathname === '/friend';
+  
   return (
   <>
-    <div className="bg-tertiary min-h-screen flex flex-col">
+    <div
+      className={`${
+        pathCheck
+          ? 'bg-[linear-gradient(to_bottom,_theme(colors.primary)_20%,_#5a7ba8_40%,_theme(colors.tertiary)_60%,_theme(colors.tertiary)_100%)]'
+          : 'bg-tertiary'
+      } min-h-screen flex flex-col`}
+    >
       <Navbar />
       <Routes>
         <Route index element={<Dashboard />}/>
@@ -20,6 +30,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/pomodoro" element={<PomodoroTimer />} />
+        <Route path="/friend" element={<FindFriend />} />
       </Routes>
       <Footer />
     </div>
---
+++ src/pages/FindFriend.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const FindFriend = () => {
+  return (
+    <div className='h-screen'>
+    </div>
+  )
+}
+
+export default FindFriend
