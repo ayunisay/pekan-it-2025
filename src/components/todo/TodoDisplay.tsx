343fa87009afed7c44cd8794acc6a39a596156ec
aryarobyy <robyaryanata12345@gmail.com>
2025-06-29 14:40:57 +0700

    Fix todo, sort by date and tile color

--- src/components/todo/TodoDisplay.tsx
+++ src/components/todo/TodoDisplay.tsx
@@ -1,69 +1,89 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import profilePic from "../../assets/images/profile.png";
 import ProgressBar from "../ProgressBar";
 import { MoreVertical } from "lucide-react";
+import type { TodoType } from "../../types/todo";
+import type { UserType } from "../../types/user";
+import { getUserById } from "../../providers/userProvider";
 
-const TodoDisplay = () => {
+interface TodoDisplayProps {
+  todo?: TodoType;
+  totalTodos: number;
+  completedTodo: number;
+}
+
+const TodoDisplay: React.FC<TodoDisplayProps> = ({
+  todo,
+  totalTodos,
+  completedTodo,
+}) => {
+  const [members, setMembers] = useState<UserType[]>([]);
+  useEffect(() => {
+    const fetchUser = async () => {
+      if (todo?.users) {
+        setMembers(todo.users);
+      }
+    };
+    fetchUser();
+  }, [todo]);
+
+  if (!todo) return;
+
   return (
     <>
-    {toDoData.map((t) => (
       <div className="flex flex-col gap-5 bg-primary w-[20rem] h-[14rem] rounded-4xl p-4 mx-5">
         <div className="flex flex-row justify-between items-center mb-2">
           <div className="flex -space-x-4">
-            {membersData.map((member, index) => (
+            {members.slice(0, 5).map((m: UserType, index: number) => (
               <MemberAvatar
-                key={member.id}
-                img={member.img}
-                altText={member.alt}
-                zIndex={membersData.length - index}
+                key={m.id}
+                img={m.avatar ?? profilePic}
+                altText={m.username}
+                zIndex={members.length - index}
               />
             ))}
           </div>
           <MoreVertical className="w-6 h-6 text-white" />
         </div>
-          <Todo 
-          progress={t.progress}
-          title={t.title}
-          />
+        <Todo
+          totalTodo={totalTodos}
+          title={todo.title}
+          completedTodo={completedTodo}
+        />
       </div>
-    ))}
     </>
   );
 };
 
-const toDoData = [
-  {id: 1, title: "Web development", progress: 20},
-  {id: 1, title: "Backend development", progress: 40},
-]
-
 type TodoProps = {
   title: string;
-  progress: number
-}
+  totalTodo: number;
+  completedTodo: number;
+};
 
-const Todo: React.FC<TodoProps> = ({ title, progress }) => {
-  return(
+const Todo: React.FC<TodoProps> = ({ title, totalTodo, completedTodo }) => {
+  const percentage = totalTodo === 0 ? 0 : parseFloat(((completedTodo / totalTodo) * 100).toFixed(1));
+  return (
     <div className="flex flex-col">
       <h3 className="text-white text-2xl font-bold">{title}</h3>
-      <ProgressBar completedPercentage={progress} tasks={progress}/>
+      <ProgressBar completedPercentage={percentage} tasks={completedTodo} />
     </div>
-  )
-}
-
-const membersData = [
-  { id: 1, img: profilePic, alt: "Member 1" },
-  { id: 2, img: profilePic, alt: "Member 2" },
-  { id: 3, img: profilePic, alt: "Member 3" },
-];
+  );
+};
 
 type MemberAvatarProps = {
   img: string;
   altText: string;
-  zIndex: number; 
+  zIndex: number;
 };
 
-const MemberAvatar: React.FC<MemberAvatarProps> = ({ img, altText, zIndex }) => {
-  return ( //nanti di cek, maks 5
+const MemberAvatar: React.FC<MemberAvatarProps> = ({
+  img,
+  altText,
+  zIndex,
+}) => {
+  return (
+    //nanti di cek, maks 5
     <div
       className="relative w-10 h-10 rounded-full overflow-hidden border-2 border-white"
       style={{ zIndex: zIndex }}
@@ -72,6 +92,5 @@ const MemberAvatar: React.FC<MemberAvatarProps> = ({ img, altText, zIndex }) => 
     </div>
   );
 };
-
 
 export default TodoDisplay;
